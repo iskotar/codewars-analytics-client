c1b5bffd5c63b6f6027a7feacac2c2024b4d4cae
Viktor Bogutskii <info@siteograf.com>
2018-08-02 12:23:06 -0700

    project base

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './redux/store';
+
+import './styles/style.css';
+import Index from './layout/Index';
+import history from './history';
+
+render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Index />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
