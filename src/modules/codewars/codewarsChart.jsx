f8e07f1780fad91b5d18ae1f00392ca042220597
Viktor Bogutskii <info@siteograf.com>
2018-08-28 13:36:42 -0700

    added avg points per task

--- src/modules/codewars/codewarsChart.jsx
+++ src/modules/codewars/codewarsChart.jsx
@@ -11,13 +11,33 @@ import {
   ResponsiveContainer
 } from 'recharts';
 
+const Label = props => {
+  const { x, y, stroke, value } = props;
+  return (
+    <text x={x} y={y} dy={-3} fill={stroke} fontSize={10} textAnchor="middle">
+      {value}
+    </text>
+  );
+};
+
+const TasksLabel = props => {
+  const { x, y, stroke, index, value, data } = props;
+  const row = data[index].data;
+  return (
+    <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">
+      {value} (
+      {(row.honor / row.codeChallenges.totalCompleted).toPrecision(3)})
+    </text>
+  );
+};
+
 const CodewarsChart = ({ codewarsAnalytics }) => {
   console.log(codewarsAnalytics);
   return (
-    <ResponsiveContainer width="100%" aspect={5.0 / 1.0}>
+    <ResponsiveContainer width="100%" aspect={3.0 / 1.0}>
       <LineChart
         data={codewarsAnalytics}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 50, right: 30, left: 0, bottom: 10 }}
       >
         <XAxis
           dataKey={'timestamp'}
@@ -25,7 +45,7 @@ const CodewarsChart = ({ codewarsAnalytics }) => {
         />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip label={'qwe'}/>
+        <Tooltip label={'qwe'} />
         <Legend />
         <Line
           type="monotone"
@@ -33,13 +53,19 @@ const CodewarsChart = ({ codewarsAnalytics }) => {
           name="Honor"
           strokeWidth={2}
           stroke="#8884d8"
+          label={<Label />}
+          dot={false}
+          isAnimationActive={false}
         />
         <Line
           type="monotone"
           dataKey="data.codeChallenges.totalCompleted"
           name="Completed Tasks"
           strokeWidth={2}
+          label={<TasksLabel data={codewarsAnalytics} />}
           stroke="#82ca9d"
+          dot={false}
+          isAnimationActive={false}
         />
       </LineChart>
     </ResponsiveContainer>
