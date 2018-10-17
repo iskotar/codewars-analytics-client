b37ff1ae37286505b0f33a51407dc8da940e8a19
iskotar <40219954+iskotar@users.noreply.github.com>
2018-10-16 21:13:18 -0700

    added h1 to home page

--- src/modules/pages/home/home.jsx
+++ src/modules/pages/home/home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Analysis of individual learning progress</div>;
+  return (
+    <div>
+      <h1>Progress Monitor</h1>
+      Analysis of individual learning progress
+    </div>
+  );
 }
